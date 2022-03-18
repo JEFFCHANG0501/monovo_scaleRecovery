8dd73cac44527703291e95b530d2760d998f890c
Jeff <jaychang0501@gmail.com>
2022-03-18 14:06:08 +0800

    extract feature point and remove vanishing points

--- src/main.py
+++ src/main.py
@@ -9,18 +9,34 @@ from mono_vo.visual_odometry import PinholeCamera, VisualOdometry
 def main():
     cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
     vo = VisualOdometry(cam)
+
+    image_id = 0
+    begin_id = 0
+    feature2d = None
+
     for img_id in range(4541):
         img = cv2.imread('/mnt/d/mono_vo/data/data_odometry_gray/dataset/sequences/00/image_1/'+str(img_id).zfill(6)+'.png', 0)
         vo.update(img, img_id)
         
-        img_c = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        if image_id > begin_id:
 
-        for i in range(vo.px_ref.shape[0]):
-            cv2.circle(img_c,(int(vo.px_ref[i,0]),int(vo.px_ref[i,1])),2,(255,0,0),-1)
-        
-        cv2.imshow('view', img_c)
-        cv2.waitKey(1)
+            feature2d = vo.feature3d[:,0:2].copy()
+            feature2d[:,0] = feature2d[:,0]*cam.fx/vo.feature3d[:,2] + cam.cx
+            feature2d[:,1] = feature2d[:,1]*cam.fx/vo.feature3d[:,2] + cam.cy
 
+            lower_feature_ids = feature2d[:,1] > 185
+            feature2d = feature2d[lower_feature_ids,:]
+            feature3d = vo.feature3d[lower_feature_ids,:]
+
+
+            img_c = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+                
+            for i in range(feature2d.shape[0]):
+                cv2.circle(img_c,(int(feature2d[i,0]),int(feature2d[i,1])),2,(255,0,0),-1)
+            
+            cv2.imshow('view', img_c)
+            cv2.waitKey(1)
+        image_id += 1
 
 if __name__ == '__main__':
     main()
--- src/mono_vo/visual_odometry.py
+++ src/mono_vo/visual_odometry.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import cv2
+from numpy.linalg import inv
 
 STAGE_FIRST_FRAME = 0
 STAGE_SECOND_FRAME = 1
@@ -20,7 +21,7 @@ def featureTracking(image_ref, image_cur, px_ref):
     state = state.reshape(state.shape[0])
     kp1 = px_ref[state == 1]
     kp2 = kp2[state == 1]
-    kp1,kp2 = feature_check(kp1,kp2)
+    # kp1,kp2 = feature_check(kp1,kp2)
     return kp1, kp2
 
 
@@ -43,20 +44,21 @@ class VisualOdometry:
         self.new_frame = None
         self.last_frame = None
         self.px_ref = None
-        self.px_ref_ = None
         self.px_cur = None
+        self.px_ref_selected = None
+        self.px_cur_selected = None
         self.cur_R = None
         self.cur_t = None
         self.camera_matrix = np.eye(3)
         self.camera_matrix[0,0] = self.camera_matrix[1,1] = self.focal
         self.camera_matrix[0,2] = cam.cx
         self.camera_matrix[1,2] = cam.cy
-        
+        self.feature3d = None
         self.detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
 
     def processFirstFrame(self):
-        self.px_ref_ = self.detector.detect(self.new_frame)
-        self.px_ref = np.array([x.pt for x in self.px_ref_], dtype=np.float32)
+        self.px_ref = self.detector.detect(self.new_frame)
+        self.px_ref = np.array([x.pt for x in self.px_ref], dtype=np.float32)
         self.frame_stage = STAGE_SECOND_FRAME
         
 
@@ -66,15 +68,34 @@ class VisualOdometry:
         E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, cameraMatrix = self.camera_matrix, method=cv2.RANSAC, prob=0.999, threshold=1.0)
         _, self.cur_R, self.cur_t, mask, points_3d = cv2.recoverPose(E, self.px_cur, self.px_ref, cameraMatrix = self.camera_matrix, distanceThresh=100)
         mask_bool = np.array(mask>0).reshape(-1)
+        points_3d_selected = points_3d [:,mask_bool].T
+        points_3d_selected[:,0] = points_3d_selected[:,0]/points_3d_selected[:,3]
+        points_3d_selected[:,1] = points_3d_selected[:,1]/points_3d_selected[:,3]
+        points_3d_selected[:,2] = points_3d_selected[:,2]/points_3d_selected[:,3]
         
+        self.feature3d = points_3d_selected[:,0:3]
+        self.px_cur_selected = self.px_cur[mask_bool,:]
+        self.px_ref_selected = self.px_ref[mask_bool,:]
+
         self.frame_stage = STAGE_DEFAULT_FRAME
         self.px_ref = self.px_cur
 
     def processFrame(self, frame_id):
         self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
+        
         E, mask_e = cv2.findEssentialMat(self.px_cur, self.px_ref, cameraMatrix = self.camera_matrix, method=cv2.RANSAC, prob=0.999, threshold=1.0)
-        _, R, t, mask, points_3d = cv2.recoverPose(E, self.px_cur, self.px_ref, cameraMatrix = self.camera_matrix, distanceThresh=100)
-        # mask_bool = np.array(mask>0).reshape(-1)
+        _, self.cur_R, self.cur_t, mask, points_3d = cv2.recoverPose(E, self.px_cur, self.px_ref, cameraMatrix = self.camera_matrix, distanceThresh=100)
+        mask_bool = np.array(mask>0).reshape(-1)
+        mask_e_bool = np.array(mask_e>0).reshape(-1)
+        mask_bool = mask_bool & mask_e_bool
+        points_3d_selected = points_3d[:,mask_bool].T
+        points_3d_selected[:,0] = points_3d_selected[:,0]/points_3d_selected[:,3]
+        points_3d_selected[:,1] = points_3d_selected[:,1]/points_3d_selected[:,3]
+        points_3d_selected[:,2] = points_3d_selected[:,2]/points_3d_selected[:,3]
+
+        self.feature3d = points_3d_selected[:,0:3]
+        self.px_cur_selected = self.px_cur[mask_bool,:]
+        self.px_ref_selected = self.px_ref[mask_bool,:]
 
         if(self.px_ref.shape[0] < kMinNumFeature):
             self.px_cur = self.detector.detect(self.new_frame)
@@ -83,7 +104,7 @@ class VisualOdometry:
         self.px_ref = self.px_cur
 
     def update(self, img, frame_id):
-        assert(img.shape[0] == self.cam.height and img.shape[1] == self.cam.width), "Frame: provided image has not the same size as the camera model or image is not grayscale"
+        assert(img.ndim ==2 and img.shape[0] == self.cam.height and img.shape[1] == self.cam.width), "Frame: provided image has not the same size as the camera model or image is not grayscale"
         self.new_frame = img
         if(self.frame_stage == STAGE_DEFAULT_FRAME):
             self.processFrame(frame_id)
